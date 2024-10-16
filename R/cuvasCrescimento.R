eb0aa613125b53eac9cc587e7b280ceb107890d2
Felipe Cadilho <93165162+FelipeCadilho@users.noreply.github.com>
2024-10-16 20:24:13 -0300

    Update cuvasCrescimento.R

--- R/cuvasCrescimento.R
+++ R/cuvasCrescimento.R
@@ -195,12 +195,13 @@ crescimento <- function(dados, c_inf, B, tzer, contador, tempoB, medida, mainNam
                 }
 
   #teste AIC - AKAIKE 
-  residuos <- data.frame(idade = c(0), ct = c(0), Bertalanffy_Residuos = c(0), Gompertz_Residuos = c(0), Logistica_Residuos = c(0))
+  residuos <- data.frame()      
   residuos$idade <- dados_curva$idade 
   residuos$ct <- dados_curva$ct 
   residuos$Bertalanffy_Residuos <- dados_curva$ct - dados_curva$bertalanffy
   residuos$Gompertz_Residuos <- dados_curva$ct - dados_curva$gompertz
   residuos$Logistica_Residuos <- dados_curva$ct - dados_curva$logistica
+  names(residuos) <- ("idade", "ct", "Bertalanffy_Residuos", "Gompertz_Residuos", "Logistica_Residuos")
       
   # Calcular a soma dos quadrados dos resíduos (SSR)
   SSR_bertalanffy <- sum(dados_curva$Bertalanffy_Residuos^2)
